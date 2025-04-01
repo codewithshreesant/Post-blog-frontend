87fbfc423fba58f74affd76c2c87ff6060163ed9
Shrisant Adhikari <149693247+codewithshreesant@users.noreply.github.com>
2025-04-01 22:18:02 +0545

    Update commentApi.js

--- src/features/comments/commentApi.js
+++ src/features/comments/commentApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const commentApi = createApi({
     reducerPath: 'commentApi',
     baseQuery: fetchBaseQuery({
-        baseUrl:'http://localhost:3000/api/comment',
+        baseUrl:'https://post-blog-backend.onrender.com/api/comment',
         credentials:'include'
     }),
     endpoints: (builder) => ({
